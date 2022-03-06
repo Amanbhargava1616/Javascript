4c3c624c5e1c33ed9b17461ffe215411d3557ca6
Aman <ama.ab36@gmail.com>
2022-03-06 19:44:45 +0530

    new

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-console.log("This content is from external JavaScript file");
+console.log("Hello world ");
+document.getElementById("para").innerHTML ="Aman bhargava here";
+document.getElementById("para").style.fontSize = "99px";
+document.getElementById("para").style.background = "red";
+document.write("This is document write ");
+alert("This is alert");
+document.querySelector("container").style.color = "blue";
