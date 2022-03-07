0ca1a29a872841f15e68ee954267e3e33f7c64d1
Aman <ama.ab36@gmail.com>
2022-03-07 17:34:20 +0530

    new

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ document.write(number1+number2);
 var name=" aman bhargava ";
 document.write(name);
 
-var marks ={
+var marks ={    //objects
     ravi:35,
     aman : 56,
     harry :453.65
@@ -18,3 +18,21 @@ var marks ={
 
 document.write(marks.aman," added to ",marks.ravi," = ",marks.aman+marks.ravi);
 console.log(marks);
+
+document.write("<br>");   // break a line
+
+var arr=[1,2,3," aman bhargava "];     //array
+
+document.write(arr[0]);
+document.write(arr[1]);
+document.write(arr[2]);
+document.write(arr[3]);
+console.log(arr);
+
+
+document.write(number1+=1);
+
+document.write("<br>");
+var radius = 40;
+const pi =3.14;
+document.write(radius*radius*pi);
