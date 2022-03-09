9e0dd65fa043731510a47b2378b70d7586bbb5a0
Aman <ama.ab36@gmail.com>
2022-03-09 16:51:34 +0530

    new

--- index.js
+++ index.js
@@ -1,2 +1,45 @@
 let arr=[1,4,3,5,6];
-document.write(arr);
+document.write("<br>",arr);
+arr.pop();
+document.write("<br>",arr);
+arr.push("aman");
+document.write("<br>",arr);
+arr.shift();
+document.write("<br>",arr);
+arr.unshift("bhargava");
+document.write("<br>",arr);
+
+document.write("<br>",arr.toString());
+
+document.write("<br>",arr.sort());
+
+document.write("<br>",arr.length);
+
+
+let str="Aman bhargava";
+
+document.write("<br>",str.length);
+
+document.write("<br>",str.indexOf("n"));
+
+document.write("<br>",str.lastIndexOf("a"));
+
+document.write("<br>",str.slice(0,3));
+
+document.write("<br>",str.replace("Aman" , "Dumpty"));
+
+document.write("<br>",str);
+
+let mydate =new Date();
+document.write("<br>",mydate);
+
+document.getElementById("para").style.background = "lightgrey ";
+
+document.getElementById("para").style.height="100px";
+document.getElementById("para").style.width="100px";
+
+let elem=document.getElementById('button');
+console.log(elem);
+
+let elem2 =document.getElementById('para');
+console.log(elem2)
