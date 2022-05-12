214154fad34e84d7b12f6baa26495e0012406ec8
Aman Bhargava <ama.ab36@gmail.com>
2022-05-12 14:27:20 +0530

    new

--- practice/demo.js
+++ practice/demo.js
@@ -1,12 +1,13 @@
 var age = prompt("Please enter your age");
-if (age > 18) {
-	age = "above";
-} else {
-	age = "Below";
-}
 document.write(age);
 try {
-	if (age <= 0) throw -1;
+	if (age > 18) {
+		age = "above";
+	} else if (age <= 0) {
+		throw -1;
+	} else {
+		age = "Below";
+	}
 } catch (error) {
 	document.write("Not valid");
 }
