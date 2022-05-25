f7abbbf2ad1a0c0a04faf4aaeda3005d3281b485
Aman Bhargava <ama.ab36@gmail.com>
2022-05-25 18:33:56 +0530

    new

--- mega-javascript/max-input.js
+++ mega-javascript/max-input.js
@@ -1,12 +1,15 @@
-let max_input = document.getElementById('max-input');
-let for_change = document.getElementById('for-change');
+const max_char = 60;
+input = document.getElementById("input");
 
+remaining_number = document.getElementById("remaining_number");
 
-function myfunction()
-{
-    const entered_value=max_input.value;
-    const remaining_char=60-entered_value.length;
-    for_change.textContent=remaining_char;
-};
+function input_char() {
+	let len = input.value.length;
 
-max_input.addEventListener('input',myfunction())
+	remaining_number.innerText = 60 - len;
+	if (len >= 50) {
+		input.style.backgroundColor = "rgb(254, 127, 127)";
+	}
+}
+
+input.addEventListener("input", input_char);
